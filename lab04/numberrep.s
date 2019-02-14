5f7bea31183c8460a44d27859021e8c9c87368fc
Sayan Paul <sayanpaul@berkeley.edu>
2019-02-14 13:50:42 -0800

    Add starter files for lab 4

---
+++ lab04/numberrep.s
@@ -0,0 +1,28 @@
+.data
+allowed_chars: .asciiz "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number: .asciiz "ABCD"
+base: 16
+
+.text
+validate_number:
+    # YOUR CODE HERE
+
+validate_number_char:
+    # a0 is the character, a1 is the base
+    add t0, a0, x0
+    addi t1, a1, -1
+    la t2, allowed_chars
+char_loop:
+    blt t1, x0, failure
+    slli t3, t1, 2
+    add t4, t2, t3
+    lw t5, 0(t4)
+    beq t0, t5, success
+    j char_loop
+success:
+    addi a0, x0, 1
+    j char_exit
+failure:
+    addi a0, x0, 0
+char_exit:
+    ret
