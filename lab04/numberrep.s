24be28ddcedeb7d0b4d234891294c9d329061c5c
Sayan Paul <sayanpaul@berkeley.edu>
2019-02-14 20:19:57 -0800

    Print return value for validate_number

--- lab04/numberrep.s
+++ lab04/numberrep.s
@@ -1,11 +1,21 @@
 .data
 allowed_chars: .asciiz "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 number: .asciiz "ABCD"
-base: 16
+base: .word 16
 
 .text
 validate_number:
     # YOUR CODE HERE
+
+    # Before this line, you should set a0 to be 1 or 0 depending on the
+    # validity of the given number and base.
+    add a1, x0, a0
+    addi a1, a1, 48
+    addi a0, x0, 11
+    ecall
+
+    addi a0, x0, 10
+    ecall #Terminate the program
 
 validate_number_char:
     # a0 is the character, a1 is the base
