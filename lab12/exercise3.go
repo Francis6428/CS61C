91896362fb346a26714204c66bceb4b7d3c50252
GodfatherSean <zehao.sean.huang@gmail.com>
2019-04-15 13:39:15 -0700

    ex3

--- lab12/exercise3.go
+++ lab12/exercise3.go
@@ -1,17 +1,22 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func return_range(n int, c chan int) {
-	// Implement Me
+	for i := 0; i < n; i++ {
+		c <- i
+	}
+	close(c)
 }
 
 func main() {
 	size := 10
 	sum := 0
 	// Implement Me
-
+	c := make(chan int, size)
+	go return_range(size, c)
+	for i := range c {
+		sum += i * i + 1
+	}
 	fmt.Println(sum)
 }
