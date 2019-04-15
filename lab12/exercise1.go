a3008717e488b5ab707a09697703d9194fc1db68
GodfatherSean <zehao.sean.huang@gmail.com>
2019-04-15 13:22:02 -0700

    ex1

--- lab12/exercise1.go
+++ lab12/exercise1.go
@@ -1,9 +1,16 @@
 // Put package name here
-
+package main
 // Put imported packages here
+import "fmt"
+import "math/rand"
 
 func main() {
 	// Put our function calls here
+	fmt.Println("Hello World!")
+	random()
 }
 
 // Put Random function here
+func random() {
+	fmt.Println("Your random number of the day is", rand.Intn(10))
+}
