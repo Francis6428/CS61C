791aeaaa56858259182ed315a032d1fc10b0d7ce
GodfatherSean <zehao.sean.huang@gmail.com>
2019-04-15 13:47:02 -0700

    ex4

--- lab12/testing/functions_test.go
+++ lab12/testing/functions_test.go
@@ -12,12 +12,27 @@ func TestAbsSumPos(t *testing.T) {
 
 func TestAbsSumNeg(t *testing.T) {
 	// Test for negatives too
+	x := []int{-100, 1, 2}
+	sum := AbsSum(x)
+	if sum != 97 {
+		t.Error("Expected 97, got", sum)
+	}
 }
 
 func TestProd(t *testing.T) {
 	// Test our product function
+	x := []int{1, 2, 3}
+	prod := Prod(x)
+	if prod != 6 {
+		t.Error("Expected 6, got", prod)
+	}
 }
 
 func TestSub(t *testing.T) {
 	// Test our subtraction function
+	x := []int{1, 2, 3}
+	sub := Sub(x)
+	if sub != -6 {
+		t.Error("Expected -6, got", sub)
+	}
 }
