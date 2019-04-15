875fc3b240dc01635c862afbdbc06224505f3c86
GodfatherSean <zehao.sean.huang@gmail.com>
2019-04-15 13:30:37 -0700

    ex2

--- lab12/exercise2.go
+++ lab12/exercise2.go
@@ -14,17 +14,36 @@ func main() {
 }
 
 func square(n int) int {
-	// Implement Me
+	return n * n
 }
 
 func print_squares(n int) {
-	// Implement Me
+	for i := 1; i <= n; i += 1 {
+		fmt.Print(square(i), " ")
+	}
+	print("\n")
 }
 
 func print_even_squares(n int) {
-	// Implement Me
+	for i := 2; i <= n; i += 2 {
+		fmt.Print(square(i), " ")
+	}
+	fmt.Print("\n")
 }
 
 func print_fibonacci(n int) {
-	// Implement Me
+	a := 1
+	b := 1
+	if (n >= 1) {
+		fmt.Print(a, " ")
+	} else if (n >= 2) {
+		fmt.Print(b, " ")
+	}
+	for i := 3; i <= n; i += 1 {
+		t := a + b
+		a = b
+		b = t
+		fmt.Print(t, " ")
+	}
+	fmt.Print("\n")
 }
